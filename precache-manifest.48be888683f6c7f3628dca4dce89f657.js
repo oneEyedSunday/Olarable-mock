881b3654ae81ffed14101064654988ac87daddc4
Idiakose O. Sunday <idiakosesunday@gmail.com>
2020-02-09 17:59:34 +0100

    Updates

---
+++ precache-manifest.48be888683f6c7f3628dca4dce89f657.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3544126f5da589c5dafe59308bb0f39e",
+    "url": "/Olarable-mock/index.html"
+  },
+  {
+    "revision": "1c8419d195c46e842574",
+    "url": "/Olarable-mock/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "daf5a9c9b60a0637151f",
+    "url": "/Olarable-mock/static/css/main.de82008c.chunk.css"
+  },
+  {
+    "revision": "1c8419d195c46e842574",
+    "url": "/Olarable-mock/static/js/2.6c428c2b.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/Olarable-mock/static/js/2.6c428c2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "daf5a9c9b60a0637151f",
+    "url": "/Olarable-mock/static/js/main.f13712cc.chunk.js"
+  },
+  {
+    "revision": "5355aa6abbccc9b4abd6",
+    "url": "/Olarable-mock/static/js/runtime-main.d8361871.js"
+  }
+]);
